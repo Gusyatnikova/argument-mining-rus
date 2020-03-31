01be5090fa32a1e878d434fa373daad0bcc2ebf3
crysnat <crysnat@mail.ru>
2020-03-31 11:31:14 +0700

    Finish reformat and collect data from brat repository

--- argument_extractor.py
+++ argument_extractor.py
@@ -1,9 +1,10 @@
 from pathlib import Path
-from brat_data_collector import bratDataCollector
+from brat_data_collector import BratDataCollector
 from bratreader.repomodel import RepoModel
 
 # will this path to brat repository be the field of UI?
-brat_repo = Path('C:\\Users\\crysn\\Desktop\\Диплом\\prog\\essays\\original')
-brat_reader = RepoModel(brat_repo)
-collector = bratDataCollector(brat_reader)
+brat_folder = Path('C:\\Users\\crysn\\Desktop\\Диплом\\prog\\essays\\original')
+brat_reader = RepoModel(brat_folder)
+collector = BratDataCollector(brat_reader)
 data = collector.collect_data()
+a = 4
--- brat_data_collector.py
+++ brat_data_collector.py
@@ -1,6 +1,40 @@
-class bratDataCollector:
+class BratDataCollector:
     def __init__(self, brat_reader):
         self.brat_reader = brat_reader
 
-    def collect_data(self ):
-        pass
+    def collect_data(self):
+        data = []
+        for i in range(1, len(self.brat_reader.documents)):
+            document = self.brat_reader.documents['essay' + str(i)]
+            data.append(self.extract_doc_features(document, 'essay' + str(i)))
+        return data
+
+    def extract_doc_features(self, brat_doc, doc_key):
+        sentence_list = []
+        label_list = []
+        link_list = []
+        doc_features = {}
+        for annotation in set(brat_doc.annotations):
+            # print(brat_doc)
+            # print("annotation :", annotation.repr)
+            # print("labels :", annotation.labels.items())
+            # print("links :", annotation.links)
+            # print("********************************************************************************")
+            sentence_list.append(annotation.repr)
+            for label in annotation.labels.items():
+                for sub_label in label:
+                    if isinstance(sub_label, list) and len(sub_label) != 0:
+                        sub_label = sub_label[0]
+                    if sub_label != [] and sub_label is not None:
+                        label_list.append(sub_label)
+            for link in annotation.links.items():
+                sub_link = link[0]
+                for sub_sub_link in link[1]:
+                    real_link = {sub_link: sub_sub_link.repr}
+                    link_list.append(real_link)
+        doc_features['key'] = doc_key
+        doc_features['sentences'] = sentence_list
+        doc_features['labels'] = label_list
+        doc_features['links'] = link_list
+        return doc_features
+
